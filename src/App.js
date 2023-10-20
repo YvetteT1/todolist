c662123bab5204b6ba5ba402a0e2eedb58877389
YvetteT1 <yvette.kmh@gmail.com>
2023-10-20 14:19:26 +0900

    todolist1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // react way
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState(["할일 1", "할일 2"]);
+  // state 변화하는 값
+  // React -> state가 변할때마다 화면을 다시 그린다.
+  // ["할일 1", "할일 2"];
+
   return (
+    // JSX (JS -> HTML)
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST</h1>
+      <div>
+        <input />
+        <button>ADD</button>
+      </div>
+      {/* DRY Don't Repeat Yourself */}
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <input type="checkbox" />
+          <span>{todo}</span>
+          <button>DEL</button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+
+/* <ul>
+<li>
+  <input type="checkbox" />
+  Task 1<button>수정</button>
+  <button>X</button>
+</li>
+<li>
+  <input type="checkbox" />
+  Task 2<button>수정</button>
+  <button>X</button>
+</li>
+</ul> */
